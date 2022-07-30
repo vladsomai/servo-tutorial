cbae38791badfb41ef69a0cc9bae1b84e58dca66
Vlad Somai <Vlad.Somai-3rdpty@brooks.com>
2022-07-31 02:27:07 +0300

    unsaved file

--- pages/tutorial/index.tsx
+++ pages/tutorial/index.tsx
@@ -20,48 +20,6 @@ const Tutorial: NextPageWithLayout = () => {
     setCurrentCommandDictionary(MotorCommands[currentChapter - 1])
   }, [currentChapter])
 
-  let currentCommandLayout: ReactElement = <></>
-  if (currentChapter == 1)
-    currentCommandLayout = (
-      <>
-        <p>you just got it!!!</p>
-      </>
-    )
-  else if (currentChapter == 2) currentCommandLayout = <></>
-  else if (currentChapter == 3) currentCommandLayout = <></>
-  else if (currentChapter == 4) currentCommandLayout = <></>
-  else if (currentChapter == 5) currentCommandLayout = <></>
-  else if (currentChapter == 6) currentCommandLayout = <></>
-  else if (currentChapter == 7) currentCommandLayout = <></>
-  else if (currentChapter == 8) currentCommandLayout = <></>
-  else if (currentChapter == 9) currentCommandLayout = <></>
-  else if (currentChapter == 10) currentCommandLayout = <></>
-  else if (currentChapter == 11) currentCommandLayout = <></>
-  else if (currentChapter == 12) currentCommandLayout = <></>
-  else if (currentChapter == 13) currentCommandLayout = <></>
-  else if (currentChapter == 14) currentCommandLayout = <></>
-  else if (currentChapter == 15) currentCommandLayout = <></>
-  else if (currentChapter == 16) currentCommandLayout = <></>
-  else if (currentChapter == 17) currentCommandLayout = <></>
-  else if (currentChapter == 18) currentCommandLayout = <></>
-  else if (currentChapter == 19) currentCommandLayout = <></>
-  else if (currentChapter == 20) currentCommandLayout = <></>
-  else if (currentChapter == 21) currentCommandLayout = <></>
-  else if (currentChapter == 22) currentCommandLayout = <></>
-  else if (currentChapter == 23) currentCommandLayout = <></>
-  else if (currentChapter == 24) currentCommandLayout = <></>
-  else if (currentChapter == 25) currentCommandLayout = <></>
-  else if (currentChapter == 26) currentCommandLayout = <></>
-  else if (currentChapter == 27) currentCommandLayout = <></>
-  else if (currentChapter == 28) currentCommandLayout = <></>
-  else if (currentChapter == 29) currentCommandLayout = <></>
-  else if (currentChapter == 30) currentCommandLayout = <></>
-  else if (currentChapter == 31) currentCommandLayout = <></>
-  else if (currentChapter == 32) currentCommandLayout = <></>
-  else if (currentChapter == 33) currentCommandLayout = <></>
-  else if (currentChapter == 34) currentCommandLayout = <></>
-  else if (currentChapter == 35) currentCommandLayout = <></>
-
   return (
     <>
       <Head>
@@ -70,9 +28,7 @@ const Tutorial: NextPageWithLayout = () => {
       <div className="flex animate__animated animate__fadeIn mt-5">
         <Chapters {...{ currentChapter, setCurrentChapter }} />
         <div className="divider divider-horizontal"></div>
-        <Main {...{ currentChapter, currentCommandDictionary }}>
-          {currentCommandLayout}
-        </Main>
+        <Main {...{ currentChapter, currentCommandDictionary }}/>
       </div>
     </>
   )
