5a9cb04dee1d125b22335c6581beac7732db5eab
Vlad Somai <Vlad.Somai-3rdpty@brooks.com>
2023-02-05 19:05:01 +0200

    add 3d motor

---
+++ pages/view/motor.tsx
@@ -0,0 +1,107 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import { ReactElement, useEffect, useRef, useState } from 'react'
+import type { NextPageWithLayout } from '../_app'
+import * as BABYLON from 'babylonjs'
+import 'babylonjs-loaders'
+
+interface ILoadingScreen {
+  //What happens when loading starts
+  displayLoadingUI: () => void
+  //What happens when loading stops
+  hideLoadingUI: () => void
+  //default loader support. Optional!
+  loadingUIBackgroundColor: string
+  loadingUIText: string
+}
+
+class CustomLoadingScreen implements ILoadingScreen {
+  //optional, but needed due to interface definitions
+  public loadingUIBackgroundColor: string = '#FFAAFF'
+  constructor(public loadingUIText: string) {}
+  public displayLoadingUI() {
+    console.log('Loading')
+  }
+
+  public hideLoadingUI() {
+    console.log('Loaded')
+  }
+}
+
+
+class Playground {
+  public static CreateScene(
+    canvas: HTMLCanvasElement,
+  ): { scene: BABYLON.Scene; engine: BABYLON.Engine } {
+    const engine = new BABYLON.Engine(canvas, true)
+    // engine.loadingScreen = new CustomLoadingScreen('Loading...')
+    // engine.displayLoadingUI()
+
+    const scene = new BABYLON.Scene(engine)
+    const camera = new BABYLON.UniversalCamera(
+      'camera1',
+      new BABYLON.Vector3(0, 5, -10),
+      scene,
+    )
+    camera.attachControl(canvas, true)
+
+    let light = new BABYLON.HemisphericLight(
+      'light1',
+      new BABYLON.Vector3(1, 1, 1),
+      scene,
+    )
+    light.intensity = 0.7
+
+    BABYLON.SceneLoader.Append('/', 'servomotor.glb', scene, function (scene) {
+      // // Create a default arc rotate camera and light.
+      scene.createDefaultCameraOrLight(true, true, true)
+      //@ts-ignore
+      scene.activeCamera!.alpha += -Math.PI / 1.2
+
+      //@ts-ignore
+      scene.activeCamera!.radius += -0.8
+    })
+
+    // engine.hideLoadingUI()
+    engine.runRenderLoop(function () {
+      scene?.render()
+    })
+
+    return { scene: scene, engine: engine }
+  }
+}
+
+const Motor: NextPageWithLayout = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+
+  useEffect(() => {
+    const { scene, engine } = Playground.CreateScene(
+      canvasRef.current as HTMLCanvasElement,
+    )
+
+    function handleResize() {
+      engine.resize()
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>3D Servo motor</title>
+      </Head>
+      <div className="text-center w-full h-full">
+        <canvas ref={canvasRef} className="w-full h-full"></canvas>
+      </div>
+    </>
+  )
+}
+
+Motor.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
+}
+
+export default Motor
