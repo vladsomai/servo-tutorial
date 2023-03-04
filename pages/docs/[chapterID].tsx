ad3768d99e0287517c4d2001cceef00bf123d91d
Vlad Somai <Vlad.Somai-3rdpty@brooks.com>
2023-03-05 01:30:09 +0200

    fix alert displayed on tutorial and cmd prot

--- pages/docs/[chapterID].tsx
+++ pages/docs/[chapterID].tsx
@@ -1,18 +1,13 @@
 import Head from "next/head";
-import {
-  ReactElement,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import Layout from "../../components/layout";
 import Chapters from "../../components/chapter-window";
 import Main, { MainWindowProps } from "../../components/main-window";
 import {
-  MotorCommandsDictionary,
-  CommandsProtocolChapter,
-  Tutorial,
-  NonCommands
+    MotorCommandsDictionary,
+    CommandsProtocolChapter,
+    Tutorial,
+    NonCommands,
 } from "../../servo-engine/motor-commands";
 import RawMotorCommands from "../../public/motor_commands.json" assert { type: "json" };
 import { useRouter } from "next/router";
@@ -21,104 +16,117 @@ import { GlobalContext } from "../_app";
 import { animated, useSpring } from "react-spring";
 
 const Docs = () => {
-  const router = useRouter();
-  const value = useContext(GlobalContext);
-  const codeAlertWasShown = useRef(false);
-  const alertTimoutHandle = useRef<NodeJS.Timeout>();
-  const MotorCommands = useRef<MotorCommandsDictionary[]>([]);
-  const [currentCommandDictionary, setCurrentCommandDictionary] =
-    useState<MotorCommandsDictionary | null>(null);
+    const router = useRouter();
+    const value = useContext(GlobalContext);
+    const codeAlertWasShown = useRef(false);
+    const alertTimoutHandle = useRef<NodeJS.Timeout>();
+    const MotorCommands = useRef<MotorCommandsDictionary[]>([]);
+    const [currentCommandDictionary, setCurrentCommandDictionary] =
+        useState<MotorCommandsDictionary | null>(null);
 
-  useEffect(() => {
-    MotorCommands.current = [
-      Tutorial,
-      CommandsProtocolChapter,
-      ...RawMotorCommands,
-    ] as MotorCommandsDictionary[];
-  }, []);
+    useEffect(() => {
+        MotorCommands.current = [
+            Tutorial,
+            CommandsProtocolChapter,
+            ...RawMotorCommands,
+        ] as MotorCommandsDictionary[];
+    }, []);
 
-  useEffect(() => {
-    if (router.isReady) {
-      const commandIdStr = router.query.chapterID;
+    useEffect(() => {
+        if (router.isReady) {
+            const commandIdStr = router.query.chapterID;
 
-      const CommandID = parseInt(commandIdStr as string);
+            const CommandID = parseInt(commandIdStr as string);
 
-      let commandFound = false;
+            let commandFound = false;
 
-      for (const command of MotorCommands.current) {
-        if (CommandID == command.CommandEnum) {
-          setCurrentCommandDictionary(command);
-          commandFound = true;
+            for (const command of MotorCommands.current) {
+                if (CommandID == command.CommandEnum) {
+                    setCurrentCommandDictionary(command);
+                    commandFound = true;
+                }
+            }
+
+            if (!commandFound) {
+                router.push("/404");
+            }
+
+            const NonCommandsIDS = [...NonCommands].map(
+                ([key, value]) => value
+            );
+
+            if (
+                !codeAlertWasShown.current &&
+                !NonCommandsIDS.includes(CommandID)
+            ) {
+                codeAlertWasShown.current = true;
+                value.alert.setTitle("Check out the 'Code examples' section!");
+                value.alert.setDescription(
+                    <>
+                        <p>
+                            The code automatically changes when you select a
+                            different axis, change the parameters or navigate
+                            through the commands.
+                        </p>
+                    </>
+                );
+                alertTimoutHandle.current = setTimeout(() => {
+                    value.alert.setShow(true);
+                }, 2000);
+                setTimeout(() => {
+                    value.alert.setShow(false);
+                }, 17200);
+            }
         }
-      }
 
-      if (!commandFound) {
-        router.push("/404");
-      }
+        return () => {
+            clearTimeout(alertTimoutHandle.current);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [router]);
 
-      if (!codeAlertWasShown.current && ![...NonCommands].includes(['',CommandID])) {
-        codeAlertWasShown.current = true;
-        value.alert.setTitle("Check out the 'Code examples' section!");
-        value.alert.setDescription(
-          <>
-            <p>
-              The code automatically changes when you select a different axis,
-              change the parameters or navigate through the commands.
-            </p>
-          </>
-        );
-        alertTimoutHandle.current = setTimeout(() => {
-          value.alert.setShow(true);
-        }, 2000);
-        setTimeout(() => {
-          value.alert.setShow(false);
-        }, 17200);
-      }
-    }
+    const [styleSpring] = useSpring(
+        () => ({
+            from: { opacity: 0 },
+            to: { opacity: 1 },
+            //   config: { duration: 1000 },
+        }),
+        []
+    );
 
-    return ()=>{clearTimeout(alertTimoutHandle.current)}
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router]);
-
-  const [styleSpring] = useSpring(
-    () => ({
-      from: { opacity: 0 },
-      to: { opacity: 1 },
-    //   config: { duration: 1000 },
-    }),
-    [],
-  )
-
-  return (
-    <>
-      <Head>
-        <title>
-          {`Command ${currentCommandDictionary?.CommandEnum} | Robots Mobots`}
-        </title>
-      </Head>
-      {currentCommandDictionary != null ? (
-        <animated.div className="flex h-full w-full" style={styleSpring}>
-          <Chapters
-            {...{
-              MotorCommands,
-              currentCommandDictionary,
-              setCurrentCommandDictionary,
-            }}
-          />
-          <Main
-            {...({
-              currentCommandDictionary,
-              MotorCommands,
-            } as MainWindowProps)}
-          />
-        </animated.div>
-      ) : null}
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>
+                    {`Command ${currentCommandDictionary?.CommandEnum} | Robots Mobots`}
+                </title>
+            </Head>
+            {currentCommandDictionary != null ? (
+                <animated.div
+                    className="flex h-full w-full"
+                    style={styleSpring}
+                >
+                    <Chapters
+                        {...{
+                            MotorCommands,
+                            currentCommandDictionary,
+                            setCurrentCommandDictionary,
+                        }}
+                    />
+                    <Main
+                        {...({
+                            currentCommandDictionary,
+                            MotorCommands,
+                        } as MainWindowProps)}
+                    />
+                </animated.div>
+            ) : null}
+        </>
+    );
 };
 
 Docs.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
+    return <Layout>{page}</Layout>;
 };
 
 export default Docs;
